2caa68906d7a21f3ed630acbbc765665028453f6
HugeFrog24 <62775760+HugeFrog24@users.noreply.github.com>
2024-12-01 16:50:49 +0100

    fix cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -181,7 +181,17 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
-CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1",
